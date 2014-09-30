deeb49dc3d93e2baeb16a916c5698a20ab45c4a8
unknown <taipan90@rambler.ru>
2014-09-30 19:44:02 +0300

    initial commit

---
+++ src/main/java/handler/HelloController.java
@@ -0,0 +1,21 @@
+package handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Taipan on 28.09.2014.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "helloworld";
+    }
+}
